443e3926ef338c10443baf5eece1496824490c75
Manish-Swamii <mswami.in.gov@gmail.com>
2025-12-03 19:13:37 +0530

    Remove unused Python scripts: problem1.py and problem2.py

--- project-ps/problem1.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-
-# For Mac, If you face error related to "pyobjc" when running the `init()` method :
-# Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
-
-engine.say("My name is vishal ")
-engine.runAndWait()
--- project-ps/problem2.py
+++
@@ -1,5 +0,0 @@
-import os
-directory_path = '/New folder'
-contents = os.listdir(directory_path)
-for item in contents:
-print(contents)
